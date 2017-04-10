dfa72a6c5c3efe32e6bdc651139040a002def84e
Louise Oram <lcoram@gmail.com>
2017-04-10 14:53:17 +0200

    Changing ctest file, and adding readme.md

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -1,8 +1,16 @@
-set(CTEST_PROJECT_NAME "Slicer")
-set(CTEST_NIGHTLY_START_TIME "3:00:00 UTC")
+## This file should be placed in the root directory of your project.
+## Then modify the CMakeLists.txt file in the root directory of your
+## project to incorporate the testing dashboard.
+##
+## # The following are required to submit to the CDash dashboard:
+##   ENABLE_TESTING()
+##   INCLUDE(CTest)
+
+set(CTEST_PROJECT_NAME "NorMIT")
+set(CTEST_NIGHTLY_START_TIME "00:00:00 UTC")
 
 set(CTEST_DROP_METHOD "http")
-set(CTEST_DROP_SITE "slicer.cdash.org")
-set(CTEST_DROP_LOCATION "/submit.php?project=Slicer4")
+set(CTEST_DROP_SITE "my.cdash.org")
+set(CTEST_DROP_LOCATION "/submit.php?project=NorMIT")
 set(CTEST_DROP_SITE_CDASH TRUE)
 
---
+++ README.md
@@ -0,0 +1,18 @@
+
+# NorMIT-Nav
+
+NorMIT-Nav is an extension for Slicer 3D. NorMIT-Nav-App is a customized version of Slicer 3D, which is compiled with the NorMIT-Nav extension. 
+
+See website for more information on [NorMIT](http://normit.no/).
+
+## Usage
+
+DISCLAIMER: NorMIT-Nav is a research tool: It is not intended for normal clinical use, and is not not FDA nor CE approved.
+
+The code is free to download and use under a MIT license.
+
+## Slicer
+
+[Slicer](http://www.slicer.org), or 3D Slicer, is a free, open source software package for visualization and image analysis. 
+
+3D Slicer is natively designed to be available on multiple platforms, including Windows, Linux and Mac Os X. 
